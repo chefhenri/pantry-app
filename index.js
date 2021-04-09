3683c207567655abc8cc602e20c58602fa83194f
Kelley Lam <kxl1360@rit.edu>
2021-04-08 20:12:54 -0400

    install yarn & successfully create table using AWS SDK for JS

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ import { name as appName } from "./app.json";
 import App from "./src/App";
 
 AppRegistry.registerComponent(appName, () => App);
+
+const { DynamoDBClient, ListTablesCommand } = require("@aws-sdk/client-dynamodb");
+
+(async () => {
+  const client = new DynamoDBClient({ region: "us-west-2" });
+  const command = new ListTablesCommand({});
+  try {
+    const results = await client.send(command);
+    console.log(results.TableNames.join("\n"));
+  } catch (err) {
+    console.error(err);
+  }
+})();
