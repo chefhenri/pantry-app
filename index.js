bc95bb4b0831aa11a11eb71655bd26caddd8e247
Henry Larson <hxl1116@rit.edu>
2021-04-20 17:06:58 -0400

    Added Apollo Client to index.js; wrapped App component in AppRoot component; removed relay.config.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 /**
  * @format
  */
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-AppRegistry.registerComponent(appName, () => App);
+import App from "./src/App";
+import { name as appName } from "./app.json";
+
+const apolloClient = new ApolloClient({
+  uri: process.env.GQL_URL,
+  cache: new InMemoryCache(),
+});
+
+const AppRoot = () => (
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent(appName, () => AppRoot);
--- relay.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  src: './src',
-  schema: './res/schema.gql',
-  exclude: ['**/node_modules/**', "**/__mocks__/**", "**/__generated__/**"]
-}
