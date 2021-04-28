27a54e650897d9549e4115d2e5181c1d85de0f8e
Henry Larson <hxl1116@rit.edu>
2021-04-28 13:53:56 -0400

    Converted ItemAmtMenu to ItemOptsDialog

---
+++ src/components/molecules/ItemOptsDialog.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { View } from "react-native";
+import { Dialog, Menu, Portal, Provider } from "react-native-paper";
+
+import { AMOUNT, getAmtIcon, getAmtText } from "../../utils/pantry.utils";
+
+const ItemOptsDialog = ({ id, visible, toggle, setIcon }) => {
+  const handlePress = (amt) => {
+    setIcon(getAmtIcon(amt));
+    toggle();
+  };
+
+  return (
+    <View>
+      <Portal>
+        <Dialog
+          visible={visible}
+        >
+          <Dialog.Title>Change Item Amount</Dialog.Title>
+          <Dialog.Content>
+            {/*Change item amount to good item*/}
+            <Menu.Item
+              icon={getAmtIcon(AMOUNT.GOOD)}
+              title={getAmtText(AMOUNT.GOOD)}
+              onPress={() => {
+                console.log(`Item ${id} amount changed to ${AMOUNT.GOOD}`);
+                handlePress(AMOUNT.GOOD);
+              }}
+            />
+
+            {/*Change item amount to okay item*/}
+            <Menu.Item
+              icon={getAmtIcon(AMOUNT.OKAY)}
+              title={getAmtText(AMOUNT.OKAY)}
+              onPress={() => {
+                console.log(`Item ${id} amount changed to ${AMOUNT.OKAY}`);
+                handlePress(AMOUNT.OKAY);
+              }}
+            />
+
+            {/*Change item amount to none item*/}
+            <Menu.Item
+              icon={getAmtIcon(AMOUNT.NONE)}
+              title={getAmtText(AMOUNT.NONE)}
+              onPress={() => {
+                console.log(`Item ${id} amount changed to ${AMOUNT.NONE}`);
+                handlePress(AMOUNT.NONE);
+              }}
+            />
+          </Dialog.Content>
+        </Dialog>
+      </Portal>
+    </View>
+  );
+};
+
+export default ItemOptsDialog;
--- src/components/organism/PantryItem.js
+++ src/components/organism/PantryItem.js
@@ -3,13 +3,14 @@ import { Colors, IconButton, List, Surface } from "react-native-paper";
 
 import styles from "../../styles/pantry.styles";
 import { getAmtIcon } from "../../utils/pantry.utils";
+import ItemOptsDialog from "../molecules/ItemOptsDialog";
 
 const PantryItem = ({ item, updateAmt }) => {
   const [starred, setStarred] = useState(false);
   const [visible, setVisible] = useState(false);
   const [amtIcon, setAmtIcon] = useState(getAmtIcon(item.amount));
 
-  const toggleMenu = () => setVisible(!visible);
+  const toggleDialog = () => setVisible(!visible);
   const handleChangeAmt = (amt) => {
     // TODO: Change item amount attribute
     setAmtIcon(getAmtIcon(amt));
@@ -29,9 +30,19 @@ const PantryItem = ({ item, updateAmt }) => {
           }}
         />)}
         right={() => (
-          // TODO: Add amount/delete icons
-          <></>
+          // TODO: Add delete icon
+          <IconButton
+            icon={amtIcon}
+            animated={true}
+            onPress={toggleDialog}
+          />
         )}
+      />
+      <ItemOptsDialog
+        id={item.id}
+        visible={visible}
+        toggle={toggleDialog}
+        setIcon={setAmtIcon}
       />
     </Surface>
   );
